706d0ab665ddd4d632c8b8fff4c7f2edfb1da62e
Dat Nguyen <ngtiendat43@gmail.com>
2025-03-19 21:30:38 +0700

    remove duplicated files

---
+++ src/server/IMS.Models/Common/Candidate_REMOTE_512.cs
@@ -0,0 +1,55 @@
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using IMS.Core.Enums;
+using IMS.Models.Security;
+
+namespace IMS.Models.Common;
+
+[Table("Candidates", Schema = "Common")]
+public class Candidate : BaseEntity, IBaseEntity
+{
+    [Required]
+    [StringLength(255)]
+    public required string Name { get; set; }
+
+    [Required]
+    [StringLength(255)]
+    public required string Email { get; set; }
+
+    [Required]
+    public required DateTime DateOfBirth { get; set; }
+
+    [Required]
+    public required string Address { get; set; }
+
+    [Required]
+    [StringLength(20)]
+    public required string Phone { get; set; }
+
+    [Required]
+    public required Gender Gender { get; set; }
+
+    [Required]
+    public required string CV_Attachment { get; set; }
+
+    public string? Note { get; set; }
+
+    public CandidateStatus Status { get; set; }
+
+    public int YearOfExperience { get; set; }
+
+    [Required]
+    public required HighestLevel HighestLevel { get; set; }
+
+    [ForeignKey(nameof(Position))]
+    public Guid PositionId { get; set; }
+
+    public Position? Position { get; set; }
+
+    [ForeignKey(nameof(RecruiterOwner))]
+    public Guid RecruiterOwnerId { get; set; }
+
+    public User? RecruiterOwner { get; set; }
+    public virtual ICollection<CandidateSkill> CandidateSkills { get; set; } = new List<CandidateSkill>();
+}
