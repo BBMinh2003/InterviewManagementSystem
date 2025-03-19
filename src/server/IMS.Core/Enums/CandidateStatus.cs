8c9b9de28adbbecdf7770501c7b54ef1e70d7876
Dat Nguyen <ngtiendat43@gmail.com>
2025-03-19 10:46:18 +0700

    Create enum status of candidate

---
+++ src/server/IMS.Core/Enums/CandidateStatus.cs
@@ -0,0 +1,18 @@
+namespace IMS.Core.Enums;
+
+public enum CandidateStatus
+{
+    Open,
+    WaitingForInterview,
+    CancelledInterview,
+    PassedInterview,
+    FailedInterview,
+    WaitingForApproval,
+    ApprovedOffer,
+    RejectedOffer,
+    WaitingForResponse,
+    AcceptedOffer,
+    DeclinedOffer,
+    CancelledOffer,
+    Banned
+}
