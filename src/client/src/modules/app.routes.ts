93617b5f5ef8f78aaf97f8ae9e5c8d755a40ee15
Dat Nguyen <ngtiendat43@gmail.com>
2025-03-20 00:39:22 +0700

    implement login interface

--- src/client/src/modules/app.routes.ts
+++ src/client/src/modules/app.routes.ts
@@ -1,7 +1,16 @@
 import { Routes } from '@angular/router';
 import { UserLayoutComponent } from './shared/layouts/user-layout/user-layout.component';
+import { LoginComponent } from './auth/login/login.component';
+import { ForgotPasswordComponent } from './auth/forgot-password/forgot-password.component';
 
 export const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent // Route riêng cho login, không nằm trong layout
+  },
+  { path: 'forgot-password', 
+    component: ForgotPasswordComponent 
+  },
   {
     path: '',
     component: UserLayoutComponent,
