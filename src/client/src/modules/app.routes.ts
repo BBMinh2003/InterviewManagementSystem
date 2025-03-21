0bffafa40ec1ecc702fff052339e1c433239dedb
Nguyễn Tiến Đạt <ngtiendat43@gmail.com>
2025-03-21 07:32:02 +0000

    Dat/fe/angular feature login

--- src/client/src/modules/app.routes.ts
+++ src/client/src/modules/app.routes.ts
@@ -1,7 +1,11 @@
 import { Routes } from '@angular/router';
 import { UserLayoutComponent } from './shared/layouts/user-layout/user-layout.component';
+import { LoginComponent } from './auth/login/login.component';
+import { ResetPasswordComponent } from './auth/reset-password/reset-password.component';
 
 export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'reset-password', component: ResetPasswordComponent },
   {
     path: '',
     component: UserLayoutComponent,
