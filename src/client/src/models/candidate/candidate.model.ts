0b99f862ecd5e8d6709ccf2d0902665f20a675ba
Dat Nguyen <ngtiendat43@gmail.com>
2025-03-27 23:29:35 +0700

    create candidate model

---
+++ src/client/src/models/candidate/candidate.model.ts
@@ -0,0 +1,20 @@
+import { BaseModel } from "../base.model";
+
+export class CandidateModel extends BaseModel {
+    public name!: string;
+    public email!: string;
+    public dateOfBirth!: Date;
+    public phone!: string;
+    public address!: string;
+    public gender!: string;
+    public cvAttachment!: string;
+    public note?: string;
+    public status!: string;
+    public yearOfExperience!: number;
+    public highestLevel!: string;
+    public positionId!: string;
+    public positionName?: string;
+    public recruiterOwnerId!: string;
+    public recruiterOwnerName!: string;
+    public candidateSkills!: string[];
+  }
